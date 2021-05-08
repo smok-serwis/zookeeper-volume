fa8403127141ad502b2e75fed54a678b4243d027
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-05-08 20:17:21 +0200

    add logging

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 docker build -t smok-serwis/zookeeper-volume .
 ID=$(docker create smok-serwis/zookeeper-volume true)
 mkdir rootfs
-docker export $id | tar -x -C rootfs/ && docker plugin create smok-serwis/zookeeper-volume .
-docker plugin push smok-serwis/zookeeper-volume
+docker export $ID | tar -x -C rootfs/ && docker plugin create smok-serwis/zookeeper-volume .
+docker plugin set smok-serwis/zookeeper-volume DEBUG=1
+docker plugin enable smok-serwis/zookeeper-volume
+#docker plugin push smok-serwis/zookeeper-volume
 
