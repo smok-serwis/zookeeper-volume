1769761a4f1c8e474af3fa270f53cb7e44539097
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-05-11 20:33:10 +0200

    fix unmounting

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /run/docker/plugins
-exec gunicorn -w 4 --bind unix:/run/docker/plugins/zookeeper-volume.sock zookeeper_plugin.run:app
+exec gunicorn -w 1 --threads 2 --bind unix:/run/docker/plugins/zookeeper-volume.sock zookeeper_plugin.run:app
